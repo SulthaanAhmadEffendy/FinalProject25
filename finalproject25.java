f17818fe2a0fc3a7233075021fce583662893ba1
sinyozr <sinyozr4@gmail.com>
2025-10-01 11:59:18 +0700

    perubahan2

---
+++ finalproject25.java
@@ -0,0 +1,9 @@
+public class  finalproject25{
+    public static void main(String[] args) {
+     
+        System.out.println("Hi,ini baris kode pertama yang dimodif User 1");
+        System.out.println("User 1 menambahkan baris baru");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 2");
+        System.out.println("User 2 menambahkan baris baru");
+    }
+}
--- sewamobil.java
+++ sewamobil.java
@@ -1,5 +1,3 @@
-package praktikumdaspro3;
-
 import java.util.Scanner;
 
 public class sewamobil {
